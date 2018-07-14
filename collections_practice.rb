70a757f50279c8fe1e81c8406c0a2b4188021554
Jessica Long <jessicalong627@gmail.com>
2018-07-14 22:45:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
-# your code goes here
+def begins_with_r(nums)
+  if nums[0] == "r"
+     false
+  elsif
+    true
+  end
+end
+
+def contain_a
+
+end
+
+def first_wa
+
+end
+
+def remove_non_strings
+
+end
+
+def count_elements
+
+end
+
+def merge_data
+
+end
+
+def find_cool(nums)
+  nums.select do |item| 
+    item.has_value?('cool')
+  end
+end
+
+def organize_schools
+
+end
