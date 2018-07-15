717db7b9b7937fee1c3458e0d2adf62eb6ad79e0
Jessica Long <jessicalong627@gmail.com>
2018-07-15 00:03:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,14 +1,14 @@
-+def begins_with_r(arr)
-+    arr.all? do |string| 
-+    string[0] == "r"
-+  end
-+end
-+    
-+def contain_a(arr)
-+    arr.select do |string|
-+    string.include?("a")
-+  end
-+end
+def begins_with_r(arr)
+    arr.all? do |string| 
+    string[0] == "r"
+  end
+end
+    
+def contain_a(arr)
+    arr.select do |string|
+    string.include?("a")
+  end
+end
 
 def first_wa
 
