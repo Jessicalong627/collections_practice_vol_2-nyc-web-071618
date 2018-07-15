7755f86957e1839ef02a6c21e50036d664bd621d
Jessica Long <jessicalong627@gmail.com>
2018-07-15 00:05:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,12 +22,18 @@ def remove_non_strings(arr)
   end
 end
 
-def count_elements
-
+def count_elements(array)
+    
+    array.uniq.each {|i| count = 0
+        array.each {|i2| if i2 == i then count += 1 end}
+        i[:count] = count}
 end
+    
 
-def merge_data
-
+def merge_data(keys, data)
+  merged = []
+  keys.each {|i| data.first.map {|k,v| if i.values[0] == k then merged << i.merge(v) end}}
+  merged
 end
 
 def find_cool(nums)
